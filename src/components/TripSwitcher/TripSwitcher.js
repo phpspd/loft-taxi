4e46e155d2721642ac3988b93a6506fbd1fdff15
p.suponev <p.suponev@tkbip.ru>
2021-11-01 19:55:48 +0300

    Prioritize styled-components styles

--- src/components/TripSwitcher/TripSwitcher.js
+++ src/components/TripSwitcher/TripSwitcher.js
@@ -32,42 +32,52 @@ const ComponentWrapper = styled.section`
 `;
 
 const ComponentContainer = styled(Container)`
-    padding: 24px;
-    position: absolute;
+    && {
+        padding: 24px;
+        position: absolute;
+    }
 `;
 
 const FormPaper = styled(Paper)`
-    width: 486px;
-    padding: 16px 0;
-    border-radius: 10px;
-    padding-bottom: 34px;
-    pointer-events: all;
-    &.cardNotFilled {
-        padding: 30px;
+    && {
+        width: 486px;
+        padding: 16px 0;
+        border-radius: 10px;
+        padding-bottom: 34px;
+        pointer-events: all;
+        &.cardNotFilled {
+            padding: 30px;
+        }
     }
 `;
 
 const FormContainer = styled(Container)`
-    padding: 0 20px;
+    && {
+        padding: 0 20px;
+    }
 `;
 
 const StyledFormControl = styled(FormControl)`
-    position: relative;
-    min-width: 100%;
-    &:first-child {
-        margin-bottom: 8px;
+    && {
+        position: relative;
+        min-width: 100%;
+        &:first-child {
+            margin-bottom: 8px;
+        }
     }
 `;
 
 const BottomPaper = styled(Paper)`
-    top: 164px;
-    left: 24px;
-    padding: 32px 46px;
-    position: absolute;
-    max-width: 486px;
-    box-sizing: border-box;
-    border-radius: 10px;
-    pointer-events: all;
+    && {
+        top: 164px;
+        left: 24px;
+        padding: 32px 46px;
+        position: absolute;
+        max-width: 486px;
+        box-sizing: border-box;
+        border-radius: 10px;
+        pointer-events: all;
+    }
 `;
 
 const RatesContainer = styled.div`
@@ -76,62 +86,78 @@ const RatesContainer = styled.div`
 `;
 
 const RatePaper = styled(Paper)`
-    width: 118px;
-    cursor: pointer;
-    height: 167px;
-    opacity: 0.6;
-    padding: 12px;
-    box-sizing: border-box;
-    transition: 250ms cubic-bezier(0.4, 0, 0.2, 1);
-    margin-right: 20px;
-    border-radius: 10px;
-    &:last-child {
-        margin-right: 0;
-    }
-    &.active {
-        opacity: 1;
-        box-shadow: 0px 3px 5px -1px rgb(0 0 0 / 20%), 0px 5px 8px 0px rgb(0 0 0 / 14%), 0px 1px 14px 0px rgb(0 0 0 / 12%);
+    && {
+        width: 118px;
+        cursor: pointer;
+        height: 167px;
+        opacity: 0.6;
+        padding: 12px;
+        box-sizing: border-box;
+        transition: 250ms cubic-bezier(0.4, 0, 0.2, 1);
+        margin-right: 20px;
+        border-radius: 10px;
+        &:last-child {
+            margin-right: 0;
+        }
+        &.active {
+            opacity: 1;
+            box-shadow: 0px 3px 5px -1px rgb(0 0 0 / 20%), 0px 5px 8px 0px rgb(0 0 0 / 14%), 0px 1px 14px 0px rgb(0 0 0 / 12%);
+        }
     }
 `;
 
 const RatePriceCaption = styled(Typography)`
-    color: #828282;
-    font-size: 11px;
-    line-height: 11px;
+    && {
+        color: #828282;
+        font-size: 11px;
+        line-height: 11px;
+    }
 `;
 
 const RatePriceValue = styled(Typography)`
-    line-height: 1;
+    && {
+        line-height: 1;
+    }
 `;
 
 const RateCardMedia = styled(CardMedia)`
-    width: 95px;
-    height: 72px;
-    margin-top: 12px;
+    && {
+        width: 95px;
+        height: 72px;
+        margin-top: 12px;
+    }
 `;
 
 const FormButton = styled(Button)`
-    margin-top: 30px;
-    padding-left: 24px;
-    padding-right: 24px;
-    font-size: 1.3rem;
-    font-weight: 400;
-    border-radius: 40px;
-    letter-spacing: 0px;
+    && {
+        margin-top: 30px;
+        padding-left: 24px;
+        padding-right: 24px;
+        font-size: 1.3rem;
+        font-weight: 400;
+        border-radius: 40px;
+        letter-spacing: 0px;
+    }
 `;
 
 const NotifyContainer = styled(Container)`
-    padding: 0 20px;
+    && {
+        padding: 0 20px;
+    }
 `;
 
 const StyledBox = styled(Box)`
-    padding: 0 24px;
-    text-align: left;
+    && {
+        padding: 0 24px;
+        text-align: left;
+    }
 `;
 
 const NotifyHeader = styled(Typography)`
-    font-weight: 700;
-    margin-bottom: 16px;
+    && {
+        font-weight: 700;
+        margin-bottom: 16px;
+    }
 `;
 
 const NotifyText = styled(Typography)`
